8ff8283aad4b8d368cbdabdcbbac58bc63be6e72
root <root@ip-10-0-0-67.ap-southeast-2.compute.internal>
2016-03-27 06:15:50 +0000

    adds support for running as a cron to report memory and swap usage

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ $1 = "cron" ]; then
+  while [ true ]
+  do
+    /aws-scripts-mon/mon-put-instance-data.pl \
+      --mem-util --mem-used --mem-avail \
+      --swap-util --swap-used \
+      --auto-scaling > /dev/null
+    sleep 30
+  done
+fi
+
 EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 
